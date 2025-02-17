0678f3b0f12f0db719e82d6de38296b4e687a55a
Nouresam95 <nouresamnoor@gmail.com>
2025-02-17 11:13:43 +0200

    Ignore .env file to keep API keys secure

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,14 @@ import 'package:smartvestment/localization/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart'; // ✅ استيراد Provider
 import 'package:smartvestment/providers/language_provider.dart'; // ✅ استيراد LanguageProvider
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+    await dotenv.load(fileName: ".env"); // تحميل المتغيرات
    runApp(
     ChangeNotifierProvider(
       create: (context) => LanguageProvider(),
